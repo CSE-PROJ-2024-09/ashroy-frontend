46660488e6f80bd9155ef16b55884719cfbf3608
Srishti Majumder <srishtimajumder2002@gmail.com>
2024-06-06 01:40:43 +0530

    touch ups done

--- src/components/layout/AppFooter.tsx
+++ src/components/layout/AppFooter.tsx
@@ -1,29 +1,23 @@
+import { FaFacebook, FaGithub } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 const AppFooter = () => {
   return (
-    <footer className="text-center text-lg-start bg-body-tertiary text-muted">
+    <footer className="text-center text-lg-start bg-dark text-white">
       <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <div className="me-5 d-none d-lg-block">
+        <div className="me-3 d-none d-lg-block">
           <span>Get connected with us on social networks:</span>
         </div>
-
         <div>
           <a href="" className="me-4 text-reset">
-            <i className="fab fa-facebook-f"></i>
+            <FaFacebook size={25} />
           </a>
           <a href="" className="me-4 text-reset">
-            <i className="fab fa-twitter"></i>
+            <FaSquareInstagram size={25} />
           </a>
           <a href="" className="me-4 text-reset">
-            <i className="fab fa-google"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-github"></i>
+            <FaGithub size={25} />
           </a>
         </div>
       </section>
@@ -33,71 +27,49 @@ const AppFooter = () => {
           <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">
-                <i className="fas fa-gem me-3"></i>Company name
+                <i className="fas fa-gem me-3"></i>ASHROY
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
+                ASHROY IS a big hub of kindness, connecting people who care with
+                opportunities to make a real difference in the lives of those
+                who need help. It's not just a website; it's a friendly place
+                where technology meets compassion, making it easier for everyone
+                to contribute to making the world a better place.
               </p>
             </div>
 
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <h6 className="text-uppercase fw-bold mb-4">Services</h6>
               <p>
-                <a href="#!" className="text-reset">
-                  Angular
-                </a>
+                <Link to={`/orphanage`} className="text-reset">
+                  Orphanage
+                </Link>
               </p>
               <p>
-                <a href="#!" className="text-reset">
-                  React
-                </a>
+                <Link to={`/animal-welfare`} className="text-reset">
+                  Animal Welfare
+                </Link>
               </p>
               <p>
-                <a href="#!" className="text-reset">
-                  Vue
-                </a>
+                <Link to={`/elderly-care`} className="text-reset">
+                  Elderly Care
+                </Link>
               </p>
               <p>
-                <a href="#!" className="text-reset">
-                  Laravel
-                </a>
-              </p>
-            </div>
-
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Help
-                </a>
+                <Link to={`/shelter-home`} className="text-reset">
+                  Homeless Shelter
+                </Link>
               </p>
             </div>
 
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
               <p>
-                <i className="fas fa-home me-3"></i> New York, NY 10012, US
+                <i className="fas fa-home me-3"></i> Siliguri, West Bengal
               </p>
               <p>
                 <i className="fas fa-envelope me-3"></i>
-                info@example.com
+                Ashroy@example.com
               </p>
               <p>
                 <i className="fas fa-phone me-3"></i> + 01 234 567 88
@@ -115,8 +87,11 @@ const AppFooter = () => {
         style={{ backgroundColor: " rgba(0, 0, 0, 0.05)" }}
       >
         © 2021 Copyright:
-        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+        <a
+          className="text-reset fw-bold"
+          href="https://ashroy-webapp.netlify.app/"
+        >
+          Ashroy
         </a>
       </div>
     </footer>
